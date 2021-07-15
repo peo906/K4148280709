b9b065ef1d564230029be2ce391035adae995e42
peo906 <peo5455@gmail.com>
2021-07-15 19:08:11 +0900

    로그인페이지

--- js/common.js
+++ js/common.js
@@ -5,4 +5,23 @@ $(function(){
             $(this).hide();
         });
     }
+
+    if ($('#loginWrap').length) {
+        $(document).on('click','.box label',function () {
+            if ($(this).hasClass('idClear')) {
+                $('input[name=idInput]').val('').focus();
+            }else{
+                $('input[name=pasWrdInput]').val('').focus();
+            }
+        });
+        $(document).on('click','.loginBtn',function () {
+            if (!$('input[name=idInput]').val() && !$('input[name=pasWrdInput]').val()) {
+                $('.error').show();
+            }else{
+                $('.error').hide();
+            }
+
+        });
+
+    }
 })
