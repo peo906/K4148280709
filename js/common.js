be5a8c98b371c83e0d297ab80a04a8d4c505f891
peo906 <peo5455@gmail.com>
2021-07-15 23:17:26 +0900

    구매페이지

--- js/common.js
+++ js/common.js
@@ -15,13 +15,41 @@ $(function(){
             }
         });
         $(document).on('click','.loginBtn',function () {
-            if (!$('input[name=idInput]').val() && !$('input[name=pasWrdInput]').val()) {
+            if (!$('input[name=idInput]').val() || !$('input[name=pasWrdInput]').val()) {
                 $('.error').show();
             }else{
                 $('.error').hide();
             }
+        });
+    }
+    if ($('#buyPageWrap').length) {
+        $(document).on('click','.btnBox ul li button',function () {
+            var price = $(this).attr('data-price');
+            $('.buyPrice').val( comma(price));
+            $('input[name=price]').val(price);
+        });
+        $(document).on('click','.buyBtn',function () {
+            var pirce = $('input[name=price]').val();
+            console.log(pirce);
+            if (pirce) {
+                if (pirce > 9999){
+                    $('#popupWrap').show();
+                }else{
+                    alert('만원 이상 구매 가능합니다.');
+                }
+            }else{
+                alert('금액을 입력해주세요.');
+            }
 
         });
-
+        function comma(str) {
+            str = String(str);
+            return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+        }
     }
+
+
+
+
+
 })
